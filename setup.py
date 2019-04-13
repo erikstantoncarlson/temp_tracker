e68d3959c32e19a0cacf98e0e1b562e43322b9e1
Erik Carlson <noirelkscar@gmail.com>
2019-04-12 20:14:03 -0700

    Convert tabs to spaces for setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-	name="temp_tracker",
-	version="0.1",
-	description="Tracks temperatures and gets their stats.",
-	url="https://github.com/erikstantoncarlson/temp_tracker",
-	author="Erik Carlson",
-	author_email="erikstantoncarlson@gmail.com",
-	license="MIT",
-	packages=["temp_tracker"],
-	test_suite="tests",
-	zip_safe=False
+    name="temp_tracker",
+    version="0.1",
+    description="Tracks temperatures and gets their stats.",
+    url="https://github.com/erikstantoncarlson/temp_tracker",
+    author="Erik Carlson",
+    author_email="erikstantoncarlson@gmail.com",
+    license="MIT",
+    packages=["temp_tracker"],
+    test_suite="tests",
+    zip_safe=False
 )
